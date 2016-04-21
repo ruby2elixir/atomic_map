d2330748476436fbe2ece4422e94b4198598b06d
Roman Heinrich <roman.heinrich@gmail.com>
2016-04-22 01:25:48 +0200

    even less code ;)

--- lib/atomic_map.ex
+++ lib/atomic_map.ex
@@ -1,13 +1,12 @@
 defmodule AtomicMap do
-  def convert(v, opts \\ [])
 
+  def convert(v, opts \\ [])
   def convert(struct=%{__struct__: type}, opts) do
     struct
     |> Map.from_struct
     |> convert(opts)
     |> Map.put(:__struct__, type)
   end
-
   def convert(map, opts) when is_map(map) do
     safe = Keyword.get(opts, :safe, true)
     map |> Enum.reduce(%{}, fn({k,v}, acc)->
@@ -23,9 +22,8 @@ defmodule AtomicMap do
     tuple |> Tuple.to_list |> convert(opts) |> List.to_tuple
   end
   def convert(v, _opts),  do: v
-  defp as_atom(s, true) when is_binary(s), do: s |> String.to_existing_atom
-  defp as_atom(s, true),                   do: s
 
+  defp as_atom(s, true)  when is_binary(s), do: s |> String.to_existing_atom
   defp as_atom(s, false) when is_binary(s), do: s |> String.to_atom
-  defp as_atom(s, false),                   do: s
+  defp as_atom(s, _),                       do: s
 end

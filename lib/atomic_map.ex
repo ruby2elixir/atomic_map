07482cbb1f7078cadba2682236b9fa5d01ce7699
Roman Heinrich <roman.heinrich@gmail.com>
2016-04-22 00:15:39 +0200

    simplify even more...

--- lib/atomic_map.ex
+++ lib/atomic_map.ex
@@ -11,8 +11,8 @@ defmodule AtomicMap do
   def convert(map, opts) when is_map(map) do
     safe = Keyword.get(opts, :safe, true)
     map |> Enum.reduce(%{}, fn({k,v}, acc)->
-      if is_binary(k),   do: k = as_atom(k, safe)
-      if is_complex?(v), do: v = convert(v, opts)
+      k = as_atom(k, safe)
+      v = convert(v, opts)
       acc |> Map.put(k, v)
     end)
   end
@@ -20,8 +20,6 @@ defmodule AtomicMap do
     list |> Enum.map(fn(x)-> convert(x, opts) end)
   end
   def convert(v, _opts),  do: v
-
-  defp is_complex?(v), do: is_map(v) || is_list(v)
   defp as_atom(s, true) when is_binary(s), do: s |> String.to_existing_atom
   defp as_atom(s, true),                   do: s
 
